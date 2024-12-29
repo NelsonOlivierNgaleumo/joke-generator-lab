935656a38b68607cd7b649bf06b342898def9290
Nelson Ngaleumo <nelsonngaleumo@yahoo.fr>
2024-12-29 15:31:00 -0600

    Joke Generator lab2 working

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,30 @@
-function Home() {
+'use client';
+
+import getJoke from '@/api/jokeData';
+import { useState } from 'react';
+
+export default function Home() {
+  const [joke, setJoke] = useState('');
+  const [jokeText, setJokeText] = useState('');
+  const [jokeButton, setJokeButton] = useState('Get a Joke');
+
+  const handleClick = () => {
+    if (jokeButton === 'Get a Joke' || jokeButton === 'Get Another Joke') {
+      getJoke().then((data) => {
+        setJoke(data);
+        setJokeText(data.setup);
+        setJokeButton('Get Punchline');
+      });
+    } else if (jokeButton === 'Get Punchline') {
+      setJokeText(
+        <>
+          {joke.setup} {joke.delivery}
+        </>,
+      );
+      setJokeButton('Get Another Joke');
+    }
+  };
+
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
@@ -9,9 +35,11 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      Welcome to Next JS!
+      Welcome to Joke Generator Lab!
+      <button type="button" onClick={handleClick}>
+        {jokeButton}
+      </button>
+      <div>{jokeText}</div>
     </div>
   );
 }
-
-export default Home;
